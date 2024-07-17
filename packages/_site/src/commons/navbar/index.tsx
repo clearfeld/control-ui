c70d44a613e9e765280a0a3673b002f3c5093fa8
Marko <49600278+clearfeld@users.noreply.github.com>
2024-07-17 00:54:13 -0400

    Clean: navbar style and return missing css properties

--- packages/_site/src/commons/navbar/index.tsx
+++ packages/_site/src/commons/navbar/index.tsx
@@ -1,6 +1,46 @@
 import * as stylex from "@stylexjs/stylex";
 import { Link } from "react-router-dom";
-import { styles } from "./styles";
+
+export const styles = stylex.create({
+  base: {
+    height: "var(--navbar-size)",
+    backgroundColor: "var(--navbar-color-bg)",
+    position: "fixed",
+    width: "100%",
+
+    /*
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr 1fr",
+      alignItems: "center",
+      padding: "0 1rem",
+    */
+
+    padding: "1rem",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    boxSizing: "border-box",
+    top: 0,
+    zIndex: 1,
+  },
+
+  link: {
+    textDecoration: "none",
+    color: "var(--text-color)",
+  },
+
+  pill: {
+    display: "flex",
+    gap: "1rem",
+    border: "2px solid var(--border-100)",
+    borderRadius: "var(--border-radius)",
+    padding: "0.5rem",
+    width: "fit-content",
+    justifyContent: "center",
+    alignItems: "center",
+    boxSizing: "border-box",
+  },
+});
 
 export default function Navbar() {
   return (
@@ -26,8 +66,8 @@ export default function Navbar() {
       </div> */}
 
       <Link to={"/playground"} {...stylex.props(styles.link)}>
-          Playground
-        </Link>
+        Playground
+      </Link>
 
       {/* <div {...stylex.props(styles.pill)}>
         <Link to={"/settings"} {...stylex.props(styles.link)}>
