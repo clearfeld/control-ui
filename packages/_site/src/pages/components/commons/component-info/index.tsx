bc14ea10c413b002bab1e00fb78e53b6dcf2360e
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-07-19 20:41:23 -0400

    Add: component right info block (#3)
    * Add: component right info block
    
    * request changes

---
+++ packages/_site/src/pages/components/commons/component-info/index.tsx
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from "react";
+
+import stylex from "@stylexjs/stylex";
+import { H5, H6 } from "@controlkit/headings";
+
+//TODO add a copy button
+interface I_ComponentInfoProps {
+  install: string;
+  npmjs: string;
+  npmTitle: string;
+  source: string;
+}
+
+interface I_InstallBlockProps {
+  text: string;
+}
+interface I_OptionProps {
+  option: "npm" | "pnpm" | "bun" | "yarn";
+}
+const componentInfoStyles = stylex.create({
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    boxSizing: "border-box",
+    minWidth: "20rem",
+  },
+
+  infoBlock: {
+    position: "sticky",
+    top: 0,
+    padding: "1rem",
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+    textWrap: "nowrap",
+  },
+
+  copyTextBlock: {
+    backgroundColor: "var(--cds-gray-200)",
+    padding: "0.5rem",
+    borderRadius: "var(--border-radius)",
+    display: "flex",
+    flexDirection: "column",
+    gap: "0.5rem",
+  },
+
+  textReset: {
+    padding: 0,
+    margin: 0,
+  },
+
+  labelValue: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "0.25rem",
+  },
+
+  optionsBar: {
+    display: "flex",
+    gap: "0.5rem",
+  },
+
+  option: {
+    cursor: "pointer",
+    opacity: "0.7",
+  },
+
+  activeOption: {
+    opacity: "1",
+  },
+});
+export default function ComponentInfo({
+  install,
+  npmjs,
+  source,
+  npmTitle,
+}: I_ComponentInfoProps) {
+  function InstallBlock({ text }: I_InstallBlockProps) {
+    const [activeOption, setActiveOption] = useState<
+      "npm" | "pnpm" | "bun" | "yarn"
+    >("npm");
+    const [installString, setInstallString] = useState("");
+
+    useEffect(() => {
+      switch (activeOption) {
+        case "npm":
+          setInstallString(`npm install ${text}`);
+          break;
+        case "pnpm":
+          setInstallString(`pnpm install ${text}`);
+          break;
+        case "bun":
+          setInstallString(`bun install ${text}`);
+          break;
+        case "yarn":
+          setInstallString(`yarn add ${text}`);
+          break;
+      }
+    }, [activeOption]);
+
+    function Option({ option }: I_OptionProps) {
+      return (
+        <H6
+          {...stylex.props(
+            componentInfoStyles.textReset,
+            componentInfoStyles.option,
+            activeOption === option && componentInfoStyles.activeOption
+          )}
+          onClick={() => setActiveOption(option)}>
+          {option}
+        </H6>
+      );
+    }
+    return (
+      <div {...stylex.props(componentInfoStyles.labelValue)}>
+        <H5>Install</H5>
+        <div {...stylex.props(componentInfoStyles.copyTextBlock)}>
+          <div {...stylex.props(componentInfoStyles.optionsBar)}>
+            <Option option="npm" />
+            <Option option="pnpm" />
+            <Option option="bun" />
+            <Option option="yarn" />
+          </div>
+          <div>
+            <p {...stylex.props(componentInfoStyles.textReset)}>
+              {installString}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div {...stylex.props(componentInfoStyles.wrapper)}>
+      <div {...stylex.props(componentInfoStyles.infoBlock)}>
+        <InstallBlock text={npmTitle} />
+
+        <div {...stylex.props(componentInfoStyles.labelValue)}>
+          <H5>Npmjs</H5>
+          <a href={npmjs}>{npmTitle}</a>
+        </div>
+
+        <div {...stylex.props(componentInfoStyles.labelValue)}>
+          <H5>Source</H5>
+          <a href={source}>Github</a>
+        </div>
+      </div>
+    </div>
+  );
+}
