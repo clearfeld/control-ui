231c6de8a6dfafb2440f9263f38bb90f4667118a
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-07-22 22:13:16 -0400

    Add: installation block for button

--- packages/_site/src/pages/components/button/index.tsx
+++ packages/_site/src/pages/components/button/index.tsx
@@ -5,6 +5,10 @@ import { Button, ButtonVariants } from "@controlkit/button";
 import CodeBlock from "../commons/code-block";
 import { PreviewBlock } from "../commons/preview_block";
 import ComponentInfo from "../commons/component-info";
+import InstallationBlock from "../commons/installation-block";
+import { H2, H4, H5 } from "@controlkit/headings";
+import { Divider } from "@controlkit/divider";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@controlkit/tabs";
 
 const buttonPageStyles = stylex.create({
   wrapper: {
@@ -39,6 +43,12 @@ const buttonPageStyles = stylex.create({
   blockWrapper: {
     padding: "1rem",
   },
+
+  stepBlock: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+  },
 });
 
 export default function ButtonPage() {
@@ -55,19 +65,57 @@ export default function ButtonPage() {
         </div> */}
 
         <div {...stylex.props(buttonPageStyles.middleColumn)}>
+          <H2>Installation</H2>
+          <Divider />
           <div>
-            <ExampleBlock
-              title="Default"
-              description="The default form of a button. Used when performing an action that will modifiy data or save new data."
-            />
-            <div {...stylex.props(buttonPageStyles.codeWrapper)}>
-              <PreviewBlock>
-                <Button>Button</Button>
-              </PreviewBlock>
+            <Tabs defaultValue="tab-1">
+              <TabsList>
+                <TabsTrigger value="tab-1">Automatic</TabsTrigger>
+                <TabsTrigger value="tab-2">Manual</TabsTrigger>
+              </TabsList>
+              <TabsContent value="tab-1">
+                <ComponentInfo
+                  install="pnpm add @controlkit/button"
+                  npmjs="https://www.npmjs.com/package/@controlkit/button"
+                  npmTitle="@controlkit/button"
+                  source="https://github.com/clearfeld/control-ui/tree/main/packages/button"
+                />
+              </TabsContent>
+              <TabsContent value="tab-2">
+                <div {...stylex.props(buttonPageStyles.stepBlock)}>
+                  <H5>1. Install the following dependencies</H5>
+                  <ComponentInfo
+                    install="pnpm add @controlkit/button"
+                    npmjs="https://www.npmjs.com/package/@controlkit/button"
+                    npmTitle="@controlkit/button"
+                    source="https://github.com/clearfeld/control-ui/tree/main/packages/button"
+                  />
+                </div>
+                <div {...stylex.props(buttonPageStyles.stepBlock)}>
+                  <H5>
+                    2. Copy and paste the following code into your project.
+                  </H5>
+                </div>
+              </TabsContent>
+            </Tabs>
+          </div>
+          <H2>Examples</H2>
+          <Divider />
 
-              <CodeBlock
-                language="tsx"
-                code={`import React from 'react';
+          <div>
+            <div>
+              <ExampleBlock
+                title="Default"
+                description="The default form of a button. Used when performing an action that will modifiy data or save new data."
+              />
+              <div {...stylex.props(buttonPageStyles.codeWrapper)}>
+                <PreviewBlock>
+                  <Button>Button</Button>
+                </PreviewBlock>
+
+                <CodeBlock
+                  language="tsx"
+                  code={`import React from 'react';
 
 import { Button } from "@controlkit/button";
 
@@ -78,23 +126,23 @@ export default function ButtonExample() {
 		</Button>
 	);
 }`}
-              />
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <ExampleBlock
-              title="Full width"
-              description="Buttons can be expanded to full width to fill its parent container."
-            />
-            <div {...stylex.props(buttonPageStyles.codeWrapper)}>
-              <PreviewBlock>
-                <Button fullWidth>Button</Button>
-              </PreviewBlock>
+            <div>
+              <ExampleBlock
+                title="Full width"
+                description="Buttons can be expanded to full width to fill its parent container."
+              />
+              <div {...stylex.props(buttonPageStyles.codeWrapper)}>
+                <PreviewBlock>
+                  <Button fullWidth>Button</Button>
+                </PreviewBlock>
 
-              <CodeBlock
-                language="tsx"
-                code={`import React from 'react';
+                <CodeBlock
+                  language="tsx"
+                  code={`import React from 'react';
 
 import { Button } from "@controlkit/button";
 
@@ -107,23 +155,23 @@ export default function ButtonExample() {
 		</Button>
 	);
 }`}
-              />
+                />
+              </div>
             </div>
-          </div>
 
-          <div>
-            <ExampleBlock
-              title="Danger"
-              description="The danger button appears as a final confirmation for a destructive action such as deleting. These are found mostly in confirmation modals."
-            />
-            <div {...stylex.props(buttonPageStyles.codeWrapper)}>
-              <PreviewBlock>
-                <Button variant={ButtonVariants.DANGER}>Button</Button>
-              </PreviewBlock>
+            <div>
+              <ExampleBlock
+                title="Danger"
+                description="The danger button appears as a final confirmation for a destructive action such as deleting. These are found mostly in confirmation modals."
+              />
+              <div {...stylex.props(buttonPageStyles.codeWrapper)}>
+                <PreviewBlock>
+                  <Button variant={ButtonVariants.DANGER}>Button</Button>
+                </PreviewBlock>
 
-              <CodeBlock
-                language="tsx"
-                code={`import React from 'react';
+                <CodeBlock
+                  language="tsx"
+                  code={`import React from 'react';
 
 import { Button, ButtonVariants } from "@controlkit/button";
 
@@ -136,21 +184,15 @@ export default function ButtonExample() {
 		</Button>
 	);
 }`}
-              />
+                />
+              </div>
             </div>
           </div>
         </div>
 
-        <ComponentInfo
-          install="pnpm add @controlkit/button"
-          npmjs="https://www.npmjs.com/package/@controlkit/button"
-          npmTitle="@controlkit/button"
-          source="https://github.com/clearfeld/control-ui/tree/main/packages/button"
-        />
         {/* <div style={{ backgroundColor: "red", width: "fit-content" }}>
           help info
         </div> */}
-
       </div>
     </div>
   );
---
+++ packages/_site/src/pages/components/commons/installation-block/index.tsx
@@ -0,0 +1,7 @@
+import stylex from "@stylexjs/stylex";
+import React from "react";
+
+const styles = stylex.create({});
+export default function InstallationBlock() {
+  return <div>InstallationBlock</div>;
+}
