5e10658c2d9a698aa78606940e6b47ec0d8582de
Marko <49600278+clearfeld@users.noreply.github.com>
2024-07-22 21:56:58 -0400

    Update: playground accordion group make multiple default tab group

--- packages/_site/src/pages/playground/accordion_group.tsx
+++ packages/_site/src/pages/playground/accordion_group.tsx
@@ -4,6 +4,7 @@ import styles from "./common_styles";
 
 import { H2 } from "@controlkit/headings";
 // import { Label } from '@controlkit/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@controlkit/tabs";
 
 import {
     Accordion,
@@ -12,14 +13,6 @@ import {
     AccordionTrigger,
 } from "@controlkit/accordion";
 
-const page_styles = stylex.create({
-    row: {
-        display: "flex",
-        flexDirection: "column",
-        gap: "1rem",
-    },
-});
-
 export default function AccordionGroup() {
     return (
         <div
@@ -27,36 +20,42 @@ export default function AccordionGroup() {
         >
             <H2>Accordion</H2>
 
-            <div
-                {...stylex.props(styles.row)}
-            >
-                <div
-                    {...stylex.props(page_styles.row)}
-                >
-                    <Accordion type="single" collapsible>
-                        <AccordionItem value="item-1">
-                            <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                            <AccordionContent>
-                                Yes. It adheres to the WAI-ARIA design pattern.
-                            </AccordionContent>
-                        </AccordionItem>
-                        <AccordionItem value="item-2">
-                            <AccordionTrigger>Is it styled?</AccordionTrigger>
-                            <AccordionContent>
-                                Yes. It comes with default styles that matches the other
-                                components&apos; aesthetic.
-                            </AccordionContent>
-                        </AccordionItem>
-                        <AccordionItem value="item-3">
-                            <AccordionTrigger>Is it animated?</AccordionTrigger>
-                            <AccordionContent>
-                                Yes. It's animated by default, but you can disable it if you prefer.
-                            </AccordionContent>
-                        </AccordionItem>
-                    </Accordion>
-                </div>
-            </div>
+            <br />
 
+            <Tabs defaultValue="multiple">
+                <TabsList>
+                    <TabsTrigger value="multiple">Multiple</TabsTrigger>
+                    <TabsTrigger value="single">Single</TabsTrigger>
+                </TabsList>
+                <TabsContent value="multiple"><AccordionExample type="multiple" /></TabsContent>
+                <TabsContent value="single"><AccordionExample type="single" /></TabsContent>
+            </Tabs>
         </div>
     )
+}
+
+function AccordionExample(props: { type: "single" | "multiple" }) {
+    return (
+        <Accordion type={props.type} collapsible>
+            <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+                <AccordionTrigger>Is it styled?</AccordionTrigger>
+                <AccordionContent>
+                    Yes. It comes with default styles that matches the other
+                    components&apos; aesthetic.
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+                <AccordionTrigger>Is it animated?</AccordionTrigger>
+                <AccordionContent>
+                    Yes. It's animated by default, but you can disable it if you prefer.
+                </AccordionContent>
+            </AccordionItem>
+        </Accordion>
+    );
 }
