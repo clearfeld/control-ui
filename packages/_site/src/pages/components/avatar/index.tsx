4329386a403b3e9ecb5c049849cd6fd75fed75a0
Marko <49600278+clearfeld@users.noreply.github.com>
2024-07-24 01:04:59 -0400

    Clean: components docs and add manual comp install

--- packages/_site/src/pages/components/avatar/index.tsx
+++ packages/_site/src/pages/components/avatar/index.tsx
@@ -41,7 +41,7 @@ const avatarPageStyles = stylex.create({
     // border: "1px solid var(--border-100)",
     borderRadius: "var(--border-radius)",
     backgroundColor: "#121212",
-    padding: "1rem",
+    // padding: "1rem",
     boxSizing: "border-box",
   },
 
@@ -92,16 +92,142 @@ export default function AvatarPage() {
                 <div {...stylex.props(avatarPageStyles.stepBlock)}>
                   <H5>1. Install the following dependencies</H5>
                   <ComponentInfo
-                    install="pnpm add @controlkit/avatar"
+                    install="pnpm add @radix-ui/react-avatar"
                     npmjs="https://www.npmjs.com/package/@controlkit/avatar"
-                    npmTitle="@controlkit/avatar"
+                    npmTitle="@radix-ui/react-avatar"
                     source="https://github.com/clearfeld/control-ui/tree/main/packages/avatar"
                   />
                 </div>
+
+                <br />
+
                 <div {...stylex.props(avatarPageStyles.stepBlock)}>
                   <H5>
                     2. Copy and paste the following code into your project.
                   </H5>
+
+                  <CodeBlock
+                    language="tsx"
+                    code={`"use client";
+
+import React, { forwardRef } from "react";
+import * as stylex from '@stylexjs/stylex';
+import * as AvatarPrimitive from "@radix-ui/react-avatar"
+
+type ExtendProps = {
+    extend?: stylex.StyleXStyles,
+    size?: AvatarSizes,
+};
+
+enum AvatarSizes {
+    SMALL = "small",
+    MEDIUM = "medium",
+    LARGE = "large",
+    XLARGE = "xlarge",
+}
+
+const styles = stylex.create({
+    avatar: {
+        display: "flex",
+        overflow: "hidden",
+        position: "relative",
+        shrink: "0",
+        borderRadius: "50%",
+        width: "2.5rem",
+        height: "2.5rem",
+        backgroundColor: "var(--avatar-background, #006699)",
+        cursor: "pointer",
+    },
+
+    avatarImage: {
+        aspectRatio: "1 / 1",
+        width: "100%",
+        height: "100%",
+        objectFit: "cover",
+    },
+
+    avatarFallback: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "50%",
+        width: "100%",
+        height: "100%",
+        color: "var(--avatar-text, #FCFCFC)",
+    },
+
+    disabled: {
+        opacity: "0.5",
+        cursor: "not-allowed",
+    },
+
+    [AvatarSizes.SMALL]: {
+        height: "1.5rem",
+        width: "1.5rem",
+        fontSize: "0.625rem",
+    },
+
+    [AvatarSizes.MEDIUM]: {
+        height: "2rem",
+        width: "2rem",
+        fontSize: "0.75rem",
+    },
+
+    [AvatarSizes.LARGE]: {
+        height: "3rem",
+        width: "3rem",
+        fontSize: "0.8625rem",
+    },
+
+    [AvatarSizes.XLARGE]: {
+        height: "4rem",
+        width: "4rem",
+        fontSize: "1rem",
+    },
+});
+
+const Avatar = forwardRef<
+    React.ElementRef<typeof AvatarPrimitive.Root>,
+    React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root> & ExtendProps
+>(({ extend, size = AvatarSizes.MEDIUM, ...props }, ref) => (
+    <AvatarPrimitive.Root
+        ref={ref}
+        {...stylex.props(
+            styles.avatar,
+            styles[size],
+            extend
+        )}
+        {...props}
+    />
+));
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = React.forwardRef<
+    React.ElementRef<typeof AvatarPrimitive.Image>,
+    React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image> & ExtendProps
+>(({ extend, ...props }, ref) => (
+    <AvatarPrimitive.Image
+        ref={ref}
+        {...stylex.props(styles.avatarImage, extend)}
+        {...props}
+    />
+));
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = React.forwardRef<
+    React.ElementRef<typeof AvatarPrimitive.Fallback>,
+    React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback> & ExtendProps
+>(({ extend, ...props }, ref) => (
+    <AvatarPrimitive.Fallback
+        ref={ref}
+        {...stylex.props(styles.avatarFallback, extend)}
+        {...props}
+    />
+));
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+export { Avatar, AvatarImage, AvatarFallback, AvatarSizes };`}
+                  />
                 </div>
               </TabsContent>
             </Tabs>
--- packages/_site/src/pages/components/divider/index.tsx
+++ packages/_site/src/pages/components/divider/index.tsx
@@ -35,7 +35,7 @@ const dividerPageStyles = stylex.create({
     // border: "1px solid var(--border-100)",
     borderRadius: "var(--border-radius)",
     backgroundColor: "#121212",
-    padding: "1rem",
+    // padding: "1rem",
     boxSizing: "border-box",
   },
 
@@ -75,10 +75,10 @@ export default function DividerPage() {
               <TabsContent value="tab-1">
                 <div {...stylex.props(dividerPageStyles.blockWrapper)}>
                   <ComponentInfo
-                    install="pnpm add @controlkit/button"
-                    npmjs="https://www.npmjs.com/package/@controlkit/button"
-                    npmTitle="@controlkit/button"
-                    source="https://github.com/clearfeld/control-ui/tree/main/packages/button"
+                    install="pnpm add @controlkit/divider"
+                    npmjs="https://www.npmjs.com/package/@controlkit/divider"
+                    npmTitle="@controlkit/divider"
+                    source="https://github.com/clearfeld/control-ui/tree/main/packages/divider"
                   />
                 </div>
               </TabsContent>
@@ -86,16 +86,86 @@ export default function DividerPage() {
                 <div {...stylex.props(dividerPageStyles.stepBlock)}>
                   <H5>1. Install the following dependencies</H5>
                   <ComponentInfo
-                    install="pnpm add @controlkit/divider"
+                    install="pnpm add @radix-ui/react-separator"
                     npmjs="https://www.npmjs.com/package/@controlkit/divider"
-                    npmTitle="@controlkit/divider"
+                    npmTitle="@radix-ui/react-separator"
                     source="https://github.com/clearfeld/control-ui/tree/main/packages/divider"
                   />
                 </div>
+
+                <br />
+
                 <div {...stylex.props(dividerPageStyles.stepBlock)}>
                   <H5>
                     2. Copy and paste the following code into your project.
                   </H5>
+
+                  <CodeBlock
+                    language="tsx"
+                    code={`"use client";
+
+import React from "react";
+import * as stylex from '@stylexjs/stylex';
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+type ExtendProps = { extend?: stylex.StyleXStyles };
+
+enum DividerVariants {
+    HORIZONTAL = "horizontal",
+    VERTICAL = "vertical"
+}
+
+const styles = stylex.create({
+    base: {
+        shrink: "0",
+        margin: 0,
+        padding: 0,
+        borderRadius: "0.25rem",
+        border: "none",
+    },
+
+    [DividerVariants.HORIZONTAL]: {
+        width: "100%",
+        borderTop: "0.0625rem solid var(--divider-color, #808080)",
+        height: "0.0625rem",
+    },
+
+    [DividerVariants.VERTICAL]: {
+        height: "100%",
+        borderRight: "0.0625rem solid var(--divider-color, #808080)",
+        width: "0.0625rem",
+    },
+});
+
+const Divider = React.forwardRef<
+    React.ElementRef<typeof SeparatorPrimitive.Root>,
+    React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root> & ExtendProps
+>(
+    (
+        { className, extend, orientation = DividerVariants.HORIZONTAL, decorative = true, ...props },
+        ref
+    ) => (
+        <SeparatorPrimitive.Root
+            ref={ref}
+            decorative={decorative}
+            orientation={orientation}
+            className={className}
+            {...stylex.props(
+                styles.base,
+                styles[orientation],
+                extend
+            )}
+            {...props}
+        />
+    )
+)
+Divider.displayName = SeparatorPrimitive.Root.displayName;
+
+export {
+    DividerVariants,
+    Divider,
+};`}
+                  />
                 </div>
               </TabsContent>
             </Tabs>
