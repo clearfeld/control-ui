65160967d8ee43d61aaa3ab3f67a86d27345a853
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-07-23 21:14:11 -0400

    delete: unused installation block

--- packages/_site/src/pages/components/commons/installation-block/index.tsx
+++
@@ -1,7 +0,0 @@
-import stylex from "@stylexjs/stylex";
-import React from "react";
-
-const styles = stylex.create({});
-export default function InstallationBlock() {
-  return <div>InstallationBlock</div>;
-}
