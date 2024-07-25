27afaa5d68ad4c8a8151094cbb7ed1a903b4cf5e
Marko <49600278+clearfeld@users.noreply.github.com>
2024-07-25 17:44:52 -0400

    Update: badge comp (atom) new styles and include forward ref

--- packages/badge/lib/index.tsx
+++ packages/badge/lib/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as stylex from "@stylexjs/stylex";
+import { type HTMLAttributes, forwardRef } from "react";
 
 type ExtendProps = { extend?: stylex.StyleXStyles };
 
@@ -16,7 +17,7 @@ const styles = stylex.create({
 		display: "flex",
 		justifyContent: "center",
 		alignItems: "center",
-		borderRadius: "100%",
+		borderRadius: "0.25rem",
 		overflow: "hidden",
 		fontWeight: "bold",
 
@@ -24,48 +25,63 @@ const styles = stylex.create({
 		minWidth: "1rem",
 		width: "fit-content",
 		fontSize: "0.75rem",
-		padding: "0.5rem",
-
-		color: "var(--badge-text-colors, #FCFCFC)",
+		padding: "0.125rem 0.375rem",
 	},
 
 	[BadgeVariants.DEFAULT]: {
-		backgroundColor: "var(--badge-default-background, #666666)",
+		color: "var(--badge-default-text, #90adbb)",
+		backgroundColor: "var(--badge-default-background, #292d33)",
 	},
 
 	[BadgeVariants.PRIMARY]: {
-		backgroundColor: "var(--badge-primary-background, #006699)",
+		color: "var(--badge-primary-text, #8db8fb)",
+		backgroundColor: "var(--badge-primary-background, #1e2b40)",
 	},
 
 	[BadgeVariants.IMPORTANT]: {
-		backgroundColor: "var(--badge-important-background, #FF2E00)",
+		color: "var(--badge-important-text, #f29d92)",
+		backgroundColor: "var(--badge-important-background, #3e241f)",
 	},
 
 	[BadgeVariants.ADDED]: {
-		backgroundColor: "var(--badge-added-background, #30AB53)",
+		color: "var(--badge-added-text, #93dfbb)",
+		backgroundColor: "var(--badge-added-background, #213d31)",
 	},
 });
 
 interface I_BadgeProps {
+	number: number;
 	variant?: BadgeVariants;
-	number?: number;
 	max?: number;
 }
 
-function Badge({ extend, variant = BadgeVariants.DEFAULT, ...props }: ExtendProps & I_BadgeProps) {
-	function GetDisplayValue(number: number, max: number | undefined): string {
-		if (max && number > max) {
-			return `${max}+`; // TODO: replace this plus with an icon
-		}
-
-		return `${number}`;
+function GetDisplayValue(number: number, max: number | undefined): string {
+	if (max && number > max) {
+		return `${max} +`; // TODO(clearfeld): replace this plus with an icon
 	}
 
-	return (
-		<div {...stylex.props(styles.base, styles[variant], extend)}>
-			{props.number && GetDisplayValue(props.number, props.max)}
-		</div>
-	);
+	return `${number}`;
 }
 
-export { Badge, BadgeVariants };
+const Badge = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement> & ExtendProps & I_BadgeProps>(
+	({ className, extend, variant = BadgeVariants.DEFAULT, ...props }, ref) => (
+		<div
+			ref={ref}
+			className={className}
+			{...stylex.props(
+				styles.base,
+				styles[variant],
+				extend
+			)}
+			{...props}
+		>
+			{props.number && GetDisplayValue(props.number, props.max)}
+		</div>
+	),
+);
+Badge.displayName = "Badge";
+
+export {
+	BadgeVariants,
+	Badge,
+};
