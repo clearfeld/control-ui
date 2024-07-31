19e882e1b4816997dc81320c3b9f777988a94c3a
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-07-31 06:30:52 -0400

    Add: cookies settings example card

---
+++ packages/_site/src/pages/examples/cookies_settings.tsx
@@ -0,0 +1,100 @@
+import { Button, ButtonVariants } from "@controlkit/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@controlkit/card";
+import { H5 } from "@controlkit/headings";
+import { Switch } from "@controlkit/switch";
+import stylex from "@stylexjs/stylex";
+import { useState } from "react";
+
+const styles = stylex.create({
+  base: {
+    display: "grid",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+
+  card: {
+    width: "480px",
+    borderRadius: "0.25rem",
+  },
+
+  cardHeader: {
+    display: "flex",
+    gap: "0.25rem",
+    padding: "1rem",
+  },
+
+  descriptionSwitch: {
+    display: "grid",
+    gridTemplateColumns: "auto 1fr",
+  },
+
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+    padding: "1rem",
+  },
+});
+export default function CookiesSettings() {
+  const [strictlyNecessary, setStrictlyNecessary] = useState(false);
+  const [functional, setFunctional] = useState(false);
+  const [performance, setPerformance] = useState(false);
+  return (
+    <div {...stylex.props(styles.base)}>
+      <Card extend={styles.card}>
+        <CardHeader extend={styles.cardHeader}>
+          <CardTitle>Cookies Settings</CardTitle>
+          <CardDescription>Manage your cookie settings here.</CardDescription>
+        </CardHeader>
+
+        <CardContent extend={styles.cardContent}>
+          <div>
+            <H5>Strictly Necessary</H5>
+            <div {...stylex.props(styles.descriptionSwitch)}>
+              <CardDescription>
+                These cookies are essential in order to use the website and use
+                its features.
+              </CardDescription>
+              <Switch
+                checked={strictlyNecessary}
+                onCheckedChange={setStrictlyNecessary}
+              />
+            </div>
+          </div>
+
+          <div>
+            <H5>Functional Cookies</H5>
+            <div {...stylex.props(styles.descriptionSwitch)}>
+              <CardDescription>
+                These cookies allow the website to provide personalized
+                functionality.
+              </CardDescription>
+              <Switch checked={functional} onCheckedChange={setFunctional} />
+            </div>
+          </div>
+
+          <div>
+            <H5>Performance Cookies</H5>
+            <div {...stylex.props(styles.descriptionSwitch)}>
+              <CardDescription>
+                These cookies help to improve the performance of the website.
+              </CardDescription>
+              <Switch checked={performance} onCheckedChange={setPerformance} />
+            </div>
+          </div>
+
+          <Button variant={ButtonVariants.ACTION}>Save preferences</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- packages/_site/src/pages/examples/index.tsx
+++ packages/_site/src/pages/examples/index.tsx
@@ -5,59 +5,67 @@ import Dashboard04 from "./Dashboard04";
 import { Auth01 } from "./auth_01";
 import { Auth02 } from "./auth_02";
 import { Auth } from "./auth";
+import CookiesSettings from "./cookies_settings";
 
 const styles = stylex.create({
-	base: {
-		width: "1366px",
-		margin: "0 auto",
-		marginTop: "2rem",
-		padding: "0 1rem",
-	},
+  base: {
+    width: "1366px",
+    margin: "0 auto",
+    marginTop: "2rem",
+    padding: "0 1rem",
+  },
 
-	container: {
-		marginTop: "1rem",
-		border: "0.0625rem solid #666666",
-		borderRadius: "0.25rem",
-		overflow: "hidden",
-		minHeight: "800px",
-		position: "relative",
-	},
+  container: {
+    marginTop: "1rem",
+    border: "0.0625rem solid #666666",
+    borderRadius: "0.25rem",
+    overflow: "hidden",
+    minHeight: "800px",
+    position: "relative",
+  },
 });
 
 export default function Examples() {
-	return (
-		<div {...stylex.props(styles.base)}>
-			<H2>Mock Layouts</H2>
+  return (
+    <div {...stylex.props(styles.base)}>
+      <H2>Mock Layouts</H2>
 
-			<br />
+      <br />
 
-			<H4>Dashboard 04 (WIP)</H4>
-			<div {...stylex.props(styles.container)}>
-				<Dashboard04 />
-			</div>
+      <H4>Dashboard 04 (WIP)</H4>
+      <div {...stylex.props(styles.container)}>
+        <Dashboard04 />
+      </div>
 
-			<br />
+      <br />
 
-			<H4>Auth</H4>
-			<div {...stylex.props(styles.container)}>
-				<Auth />
-			</div>
+      <H4>Auth</H4>
+      <div {...stylex.props(styles.container)}>
+        <Auth />
+      </div>
 
-			<br />
+      <br />
 
-			<H4>Auth 01 (WIP)</H4>
-			<div {...stylex.props(styles.container)}>
-				<Auth01 />
-			</div>
+      <H4>Auth 01 (WIP)</H4>
+      <div {...stylex.props(styles.container)}>
+        <Auth01 />
+      </div>
 
-			<br />
+      <br />
 
-			<H4>Auth 02 (WIP)</H4>
-			<div {...stylex.props(styles.container)}>
-				<Auth02 />
-			</div>
+      <H4>Auth 02 (WIP)</H4>
+      <div {...stylex.props(styles.container)}>
+        <Auth02 />
+      </div>
 
-			<br />
-		</div>
-	);
+      <br />
+
+      <H4>Cookies Setting Card (WIP)</H4>
+      <div {...stylex.props(styles.container)}>
+        <CookiesSettings />
+      </div>
+
+      <br />
+    </div>
+  );
 }
