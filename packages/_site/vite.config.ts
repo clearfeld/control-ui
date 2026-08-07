8d3f00c5c2bef504d2d2f65bdeef85fc93679728
Marko <49600278+clearfeld@users.noreply.github.com>
2026-08-07 01:31:27 -0400

    Chore: bump _site to latest dpes

--- packages/_site/vite.config.ts
+++ packages/_site/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-// import stylexPlugin from "@stylexjs/rollup-plugin";
-// import styleX from "vite-plugin-stylex";
+import react from "@vitejs/plugin-react";
 import stylex from '@stylexjs/unplugin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		stylex.vite({
@@ -14,9 +11,7 @@ export default defineConfig({
 		react(),
 	],
 
-	esbuild: {
-		supported: {
-			"top-level-await": true,
-		},
-	},
+	build: {
+    	target: "esnext"
+  	},
 });
