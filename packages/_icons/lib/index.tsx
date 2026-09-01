293b4146c1f4a7a4bdd1c521a6aa5d7ab5a78d38
Aaron Patterson <aaronpatters@gmail.com>
2026-09-01 14:31:12 -0400

    Spelling grammar page fixed (#41)
    * Initializing changes for the language page
    
    * Adding missing App.tsx
    
    * Small fixes and simplification
    
    * Removing unecessary ampersand

--- packages/_icons/lib/index.tsx
+++ packages/_icons/lib/index.tsx
@@ -4,10 +4,14 @@ import { CloseIcon } from "./icons/close.tsx";
 import { ChevronUpIcon } from "./icons/chevronUp.tsx";
 import { ChevronDownIcon } from "./icons/chevronDown.tsx";
 import { ArrowRight } from "./icons/arrowRight.tsx";
+import { CloseCircleIcon } from "./icons/closeCircle.tsx";
+import { CheckCircleIcon } from "./icons/checkCircle.tsx";
 
 export {
 	CloseIcon,
 	ChevronUpIcon,
 	ChevronDownIcon,
 	ArrowRight,
+	CloseCircleIcon,
+	CheckCircleIcon,
 };
